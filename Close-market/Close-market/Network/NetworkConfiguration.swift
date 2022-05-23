c7420173e50b68757ba828d46c16a8b1726bee40
Lingo <llingo@kakao.com>
2022-05-24 01:44:30 +0900

    feat: NetworkConfiguration 프로토콜 및 DefaultNetworkConfiguration 구현체 구현

--- Close-market/Close-market/Network/NetworkConfiguration.swift
+++ Close-market/Close-market/Network/NetworkConfiguration.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+protocol NetworkConfiguration {
+  var baseURL: URL { get }
+  var headers: [String: String] { get }
+  var queries: [String: String] { get }
+}
+
+struct DefaultNetworkConfiguration: NetworkConfiguration {
+  let baseURL: URL
+  let headers: [String : String]
+  let queries: [String : String]
+  
+  init(
+    baseURL: URL,
+    headers: [String: String] = [:],
+    queries: [String: String] = [:]
+  ) {
+    self.baseURL = baseURL
+    self.headers = headers
+    self.queries = queries
+  }
+}
