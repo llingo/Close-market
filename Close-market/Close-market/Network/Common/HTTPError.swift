060baf3a648cc413270a56c749d5fcd34d6b6cbf
Lingo <llingo@kakao.com>
2022-05-24 02:55:22 +0900

    chore: HTTPMethod, HTTPError 파일 생성

---
+++ Close-market/Close-market/Network/Common/HTTPError.swift
@@ -0,0 +1,8 @@
+//
+//  HTTPError.swift
+//  Close-market
+//
+//  Created by Lingo on 2022/05/24.
+//
+
+import Foundation
---
+++ Close-market/Close-market/Network/Common/HTTPMethod.swift
@@ -0,0 +1,8 @@
+//
+//  HTTPMethod.swift
+//  Close-market
+//
+//  Created by Lingo on 2022/05/24.
+//
+
+import Foundation
