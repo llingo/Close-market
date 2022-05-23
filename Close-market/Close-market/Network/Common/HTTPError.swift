b9147a89e1f304ee2e0d0a0267d157132ad09799
Lingo <llingo@kakao.com>
2022-05-24 03:16:05 +0900

    feat: HTTPError 에러 타입 구현

--- Close-market/Close-market/Network/Common/HTTPError.swift
+++ Close-market/Close-market/Network/Common/HTTPError.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+enum HTTPError: LocalizedError {
+  case BadRequest
+  case NotFound
+  case InternalServer
+  
+  var errorDescription: String? {
+    switch self {
+    case .BadRequest:
+      return "BAD_REQUEST_ERROR"
+    case .NotFound:
+      return "NOT_FOUND_ERROR"
+    case .InternalServer:
+      return "INTERNAL_SERVER_ERROR"
+    }
+  }
+}
+
