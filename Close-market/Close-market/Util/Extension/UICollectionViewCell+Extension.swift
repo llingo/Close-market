ea1188b279c5e7da275b2a37b4f3b47aeba21436
Lingo <llingo@kakao.com>
2022-05-24 19:22:50 +0900

    feat: UICollectionViewCell+Extension identifier 구현

--- Close-market/Close-market/Util/Extension/UICollectionViewCell+Extension.swift
+++ Close-market/Close-market/Util/Extension/UICollectionViewCell+Extension.swift
@@ -5,4 +5,10 @@
 //  Created by Lingo on 2022/05/24.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionViewCell {
+  static var identifier: String {
+    return String(describing: self)
+  }
+}
