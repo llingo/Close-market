0904ccbdb698caef4dbc53e34d1a673994ed74da
Lingo <llingo@kakao.com>
2022-05-24 23:32:16 +0900

    feat: DefaultProductRepository 타입 구현

--- Close-market/Close-market/Data/Repositories/DefaultProductRepository.swift
+++ Close-market/Close-market/Data/Repositories/DefaultProductRepository.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+final class DefaultProductRepository: ProductRepository {
+  private let service: NetworkService
+  
+  init(service: NetworkService) {
+    self.service = service
+  }
+  
+  func fetchProductAll(
+    pageNumber: Int,
+    itemsPerPage: Int,
+    completion: @escaping (Result<[Product], NetworkError>) -> Void
+  ) {
+    let queries = ["page_no": pageNumber, "items_per_page": itemsPerPage]
+    let endpoint = APIEndpoints.getProductAll(queries: queries)
+    let _ = service.request(endpoint: endpoint) { result in
+      switch result {
+      case let .success(data):
+        if let productResponseDTO = try? JSONDecoder().decode(ProductResponseDTO.self, from: data) {
+          let products = productResponseDTO.products.map { $0.toDomain() }
+          completion(.success(products))
+        }
+      case let .failure(error):
+        print(error)
+      }
+    }
+  }
+  
+  func fetchProductOne(
+    productId: Int,
+    completion: @escaping (Result<Product, NetworkError>) -> Void
+  ) {
+    let endpoint = APIEndpoints.getProductOne(productId: productId)
+    let _ = service.request(endpoint: endpoint) { result in
+      switch result {
+      case let .success(data):
+        print(data)
+      case let .failure(error):
+        print(error)
+      }
+    }
+  }
+}
