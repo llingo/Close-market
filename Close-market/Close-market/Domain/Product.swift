6c12d01890a677f1bfc4eceeccf0e9e79edeebd6
Lingo <llingo@kakao.com>
2022-05-24 20:56:29 +0900

    feat: Product 타입 구현

--- Close-market/Close-market/Domain/Product.swift
+++ Close-market/Close-market/Domain/Product.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Product: Encodable {
+  private enum CodingKeys: String, CodingKey {
+    case name
+    case descriptions
+    case price
+    case currency
+    case discountedPrice = "discounted_price"
+    case stock
+    case secret
+  }
+  
+  enum Currency: String, Encodable {
+    case KRW, USD
+  }
+  
+  let name: String
+  let descriptions: String
+  let price: Int
+  let currency: Currency
+  let discountedPrice: Int?
+  let stock: Int?
+  let secret: String
+}
