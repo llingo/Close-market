fbe363a9764228f2439f503612a840cdbd2fbbc3
Lingo <llingo@kakao.com>
2022-05-24 20:44:53 +0900

    feat: 기존의 ProductResponseDTO -> ProductDTO로 변경 및 ProductResponseDTO 타입 구현

--- Close-market/Close-market/Data/DTO/ProductResponseDTO.swift
+++ Close-market/Close-market/Data/DTO/ProductResponseDTO.swift
@@ -9,6 +9,29 @@ import Foundation
 
 struct ProductResponseDTO: Decodable {
   private enum CodingKeys: String, CodingKey {
+    case offset, limit
+    case pageNumber = "page_no"
+    case itemsPerPage = "items_per_page"
+    case totalCount = "total_count"
+    case lastPage = "last_page"
+    case hasNext = "has_next"
+    case hasPrev = "has_prev"
+    case products = "pages"
+  }
+  
+  let pageNumber: Int
+  let itemsPerPage: Int
+  let totalCount: Int
+  let offset: Int
+  let limit: Int
+  let lastPage: Int
+  let hasNext: Bool
+  let hasPrev: Bool
+  let products: [ProductDTO]
+}
+
+struct ProductDTO: Decodable {
+  private enum CodingKeys: String, CodingKey {
     case id, name, thumbnail, currency, price, stock
     case vendorId = "vendor_id"
     case bargainPrice = "bargain_price"
