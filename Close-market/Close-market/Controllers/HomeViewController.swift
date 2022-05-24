479de661bdad1613f24eb83ac7683509cef0735b
Lingo <llingo@kakao.com>
2022-05-24 19:21:09 +0900

    feat: Home 화면 UI 구현

--- Close-market/Close-market/Controllers/HomeViewController.swift
+++ Close-market/Close-market/Controllers/HomeViewController.swift
@@ -8,7 +8,42 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
+  @IBOutlet weak var regionLabel: UIButton!
+  @IBOutlet weak var collectionView: UICollectionView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    collectionView.delegate = self
+  }
+}
+
+// MARK: - Delegate
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    sizeForItemAt indexPath: IndexPath
+  ) -> CGSize {
+    return CGSize(width: view.frame.width, height: view.frame.height * 0.17)
+  }
+}
+
+// MARK: - DataSource
+
+extension HomeViewController: UICollectionViewDataSource {
+  func collectionView(
+    _ collectionView: UICollectionView,
+    numberOfItemsInSection section: Int
+  ) -> Int {
+    return 20
+  }
+  
+  func collectionView(
+    _ collectionView: UICollectionView,
+    cellForItemAt indexPath: IndexPath
+  ) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CELL", for: indexPath)
+    return cell
   }
 }
