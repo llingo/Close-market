23c9e75db760cfa03afcb01417a041965f435428
Lingo <llingo@kakao.com>
2022-05-24 20:39:03 +0900

    feat: ProductResponseDTO 타입 생성

--- Close-market/Close-market/Data/DTO/ProductResponseDTO.swift
+++ Close-market/Close-market/Data/DTO/ProductResponseDTO.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct ProductResponseDTO: Decodable {
+  private enum CodingKeys: String, CodingKey {
+    case id, name, thumbnail, currency, price, stock
+    case vendorId = "vendor_id"
+    case bargainPrice = "bargain_price"
+    case discountedPrice = "discounted_price"
+    case createdAt = "created_at"
+    case issuedAt = "issued_at"
+  }
+  
+  enum Currency: String, Decodable {
+    case KRW, USD
+  }
+
+  let id: Int
+  let vendorId: Int
+  let name: String
+  let thumbnail: String
+  let currency: Currency
+  let price: Int
+  let bargainPrice: Int
+  let discountedPrice: Int
+  let stock: Int
+  let createdAt: String
+  let issuedAt: String
+}
