b31a345e1846a2aa7f4149d70388990df03c3511
Lingo <llingo@kakao.com>
2022-05-24 21:32:25 +0900

    feat: Domain - Product 타입 구현

--- Close-market/Close-market/Domain/Product.swift
+++ Close-market/Close-market/Domain/Product.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Product {
+  let name: String
+  let thumbnail: String
+  let currency: Currency
+  let price: Int
+  let bargainPrice: Int
+  let discountedPrice: Int
+  let stock: Int
+  let createdAt: Date
+  let issuedAt: Date
+}
