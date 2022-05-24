2780ca034d337f18422c6ab5b2c11a2b6496cd07
Lingo <llingo@kakao.com>
2022-05-24 13:13:55 +0900

    feat: UIFont NotoSans 폰트 적용 및 다이나믹 타입 기능 구현

--- Close-market/Close-market/Util/Extension/UIFont+Extension.swift
+++ Close-market/Close-market/Util/Extension/UIFont+Extension.swift
@@ -5,4 +5,25 @@
 //  Created by Lingo on 2022/05/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+  enum FontStyle: String {
+    case Black
+    case Bold
+    case DemiLight
+    case Light
+    case Medium
+    case Regular
+    case Thin
+  }
+  
+  static func notoSans(size: CGFloat, style: FontStyle = .Regular) -> UIFont? {
+    return UIFont(name: "NotoSansCJKkr-\(style)", size: size)
+  }
+  
+  func toDynamical(textStyle: TextStyle = .body) -> UIFont {
+    let font = UIFontMetrics(forTextStyle: textStyle)
+    return font.scaledFont(for: self)
+  }
+}
