94e7a1d643dc686d1c74fe3cf09386be96308737
Lingo <llingo@kakao.com>
2022-05-24 22:07:26 +0900

    feat: APIEndpoints 타입 메서드 구현

--- Close-market/Close-market/Data/DTO/APIEndpoints.swift
+++ Close-market/Close-market/Data/DTO/APIEndpoints.swift
@@ -6,3 +6,78 @@
 //
 
 import Foundation
+
+struct APIEndpoints {
+  static func getHealthChecker() -> Endpoint {
+    return Endpoint(path: "healthChecker", method: .Get)
+  }
+  
+  static func getProductOne(
+    productId: Int
+  ) -> Endpoint {
+    return Endpoint(
+      path: "api/products/\(productId)",
+      method: .Get
+    )
+  }
+  
+  static func getProductAll(
+    queries: [String: Any]
+  ) -> Endpoint {
+    return Endpoint(
+      path: "api/products",
+      method: .Get,
+      queries: queries
+    )
+  }
+  
+  static func createProduct(
+    payload: Data?,
+    headers: [String : String]
+  ) -> Endpoint {
+    return Endpoint(
+      path: "api/products",
+      method: .Post,
+      headers: headers,
+      payload: payload
+    )
+  }
+  
+  static func updateProduct(
+    productId: Int,
+    payload: Data?,
+    headers: [String : String]
+  ) -> Endpoint {
+    return Endpoint(
+      path: "api/products/\(productId)",
+      method: .Patch,
+      headers: headers,
+      payload: payload
+    )
+  }
+  
+  static func searchProductSecretKey(
+    productId: Int,
+    payload: Data?,
+    headers: [String : String]
+  ) -> Endpoint {
+    return Endpoint(
+      path: "api/products/\(productId)/secret",
+      method: .Post,
+      headers: headers,
+      payload: payload
+    )
+  }
+  
+  static func deleteProduct(
+    productId: Int,
+    secretKey: String,
+    headers: [String : String]
+  ) -> Endpoint {
+    return Endpoint(
+      path: "api/products/\(productId)/\(secretKey)",
+      method: .Delete,
+      headers: headers
+    )
+  }
+}
